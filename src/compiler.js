d46ff12480be50bbd5678e40f72f2449bb9a12cb
siminino <victor.areas@corp.globo.com>
2017-08-17 15:18:23 -0300

    add compiler code

---
+++ src/compiler.js
@@ -0,0 +1,53 @@
+import sourceMapSupport from "source-map-support";
+
+import fs from "fs";
+import { basename, extname } from "path";
+
+import { compile } from "svelte";
+import Babel from "babel-core";
+
+const BabelConfig = {
+  presets: [
+    ["env", {
+      targets: {
+        node: "current"
+      }
+    }]
+  ]
+};
+
+function sanitize(input) {
+	return basename(input)
+		.replace(extname(input), '')
+		.replace(/[^a-zA-Z_$0-9]+/g, '_')
+		.replace(/^_/, '')
+		.replace(/_$/, '')
+		.replace(/^(\d)/, '_$1');
+}
+
+function capitalize(str) {
+	return str[0].toUpperCase() + str.slice(1);
+}
+
+function loader(m, filename) {
+  const code = fs.readFileSync(filename, "utf8");
+  const options = {};
+
+  let svelteResult;
+  let result;
+
+  options.filename = filename;
+  options.format = 'es';
+  if (!options.name) options.name = capitalize(sanitize(options.filename));
+
+  try {
+      svelteResult = compile(code, options);
+      result = Babel.transform(svelteResult.code, BabelConfig);
+  } catch (err) {
+      throw Error(err.toString() + '\n' + err.frame);
+  }
+
+  m._compile(result.code, filename);
+}
+
+require.extensions[".html"] = loader;
